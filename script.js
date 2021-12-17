2bf029af797561de04817c4998685a9541446ba5
dk <dkdasuya7@gmail.com>
2021-12-18 01:19:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('removed').textContent = '<<';
+
+document.getElementById('displayed').addEventListener('click',function(){
+
+})
+
+document.getElementById('removed').addEventListener('click',function(){
+
+})
+
+document.getElementById('input-file').addEventListener('change',function(event){
+
+})
