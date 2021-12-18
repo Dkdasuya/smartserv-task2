1c8ab5654290cef2677ee7dd59f86a27aa69155e
dk <dkdasuya7@gmail.com>
2021-12-18 11:58:28 +0530

    fetching json data from file

--- script.js
+++ script.js
@@ -9,5 +9,20 @@ document.getElementById('removed').addEventListener('click',function(){
 })
 
 document.getElementById('input-file').addEventListener('change',function(event){
-
+    var result;
+    const file_to_read = document.getElementById("input-file").files[0];
+    const fileread = new FileReader();
+    fileread.onload = function(e) {
+      const content = e.target.result;
+      result = JSON.parse(content); 
+      console.log(result);
+      displayInAvailableFields(result);
+    };
+    fileread.readAsText(file_to_read);
+    
 })
+
+function displayInAvailableFields(jsonData){
+    console.log(jsonData.products[0]);
+}
+
